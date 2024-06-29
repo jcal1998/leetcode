012f4fb4b40f07599a98e1d051a636e1ebed7cd7
José Carlos Lee <josecarlos_lee@hotmail.com>
2024-06-28 23:45:34 -0300

    generate parentheses

--- 22. Generate Parentheses.ts
+++ 22. Generate Parentheses.ts
@@ -1,9 +1,23 @@
 function generateParenthesis(n: number): string[] {
-    
-};
+  const result: string[] = [];
+  let left = 0,
+    right = 0;
 
-()
+  const dfs = (current: string, left: number, right: number) => {
+    if (left === n && right === n) {
+      result.push(current);
+    }
 
-()() (())
+    if (left < n) {
+      dfs(current + "(", left + 1, right);
+    }
 
-()()()  ((()))
+    if (left > right) {
+      dfs(current + ")", left, right + 1);
+    }
+  };
+
+  dfs("", left, right);
+
+  return result;
+}
