bc8e03229b1981a0ca7fa1178c9301ec173da02e
José Carlos Lee <josecarlos_lee@hotmail.com>
2024-07-02 01:34:24 -0300

    remove duplicates

---
+++ 26. Remove Duplicates from Sorted Array.ts
@@ -0,0 +1,13 @@
+function removeDuplicates(nums: number[]): number {
+  const result: number[] = [];
+  let left = 1;
+
+  for (let right = 1; right < nums.length; right++) {
+    if (nums[right] !== nums[right - 1]) {
+      nums[left] = nums[right];
+      left++;
+    }
+  }
+
+  return left;
+}
