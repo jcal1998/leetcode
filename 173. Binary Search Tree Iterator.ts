88bac5bcdf043f2d35743a1e04e221be012505d3
José Carlos Lee <josecarlos_lee@hotmail.com>
2024-07-12 18:36:06 -0300

    binary search tree iterator

--- 173. Binary Search Tree Iterator.ts
+++ 173. Binary Search Tree Iterator.ts
@@ -45,3 +45,55 @@ class BSTIterator {
  * var param_1 = obj.next()
  * var param_2 = obj.hasNext()
  */
+
+// chat gpt
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class BSTIterator {
+  stack: Array<TreeNode>;
+  constructor(root: TreeNode | null) {
+    this.stack = [];
+    let cur = root;
+    while (cur) {
+      this.stack.push(cur);
+      cur = cur.left;
+    }
+  }
+
+  next(): number {
+    let temp = this.stack.pop();
+    if (temp.right) {
+      let cur = temp.right;
+      while (cur) {
+        this.stack.push(cur);
+        cur = cur.left;
+      }
+    }
+
+    return temp.val;
+  }
+
+  hasNext(): boolean {
+    return this.stack.length > 0;
+  }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
