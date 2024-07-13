fb710697a55ded4a8550e373f8bd4c70e518e428
José Carlos Lee <josecarlos_lee@hotmail.com>
2024-07-12 22:19:39 -0300

    binary tree level order traversal

---
+++ 102. Binary Tree Level Order Traversal.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) return [];
+  let result = [];
+  let queue = [root];
+
+  while (queue.length) {
+    const levelLen = queue.length;
+    const levelNodes = [];
+    for (let i = 0; i < levelLen; i++) {
+      const node = queue.shift();
+
+      levelNodes.push(node.val);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    result.push(levelNodes);
+  }
+
+  return result;
+}
---
+++ 637. Average of Levels in Binary Tree.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function averageOfLevels(root: TreeNode | null): number[] {
+  const result = [];
+  const queue = [root];
+
+  while (queue.length) {
+    const levelLen = queue.length;
+    let sum = 0;
+    for (let i = 0; i < levelLen; i++) {
+      const node = queue.shift();
+      sum += node.val;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(sum / levelLen);
+  }
+
+  return result;
+}
