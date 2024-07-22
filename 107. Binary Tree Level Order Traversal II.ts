d375b6f6e8577c7f35bdd88edc899b647c159a34
José Carlos Lee <josecarlos_lee@hotmail.com>
2024-07-21 23:56:02 -0300

    binary tree level order traversal II

---
+++ 107. Binary Tree Level Order Traversal II.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+
+  while (queue.length) {
+    const len = queue.length;
+    const levelArr = [];
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift();
+      levelArr.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    result.push(levelArr);
+  }
+
+  return result.reverse();
+}
