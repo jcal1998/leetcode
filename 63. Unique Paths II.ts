62d063fd5f39f202ecd864b313c653734316f6a2
José Carlos Lee <josecarlos_lee@hotmail.com>
2024-07-26 21:06:21 -0300

    unique path II

--- 63. Unique Paths II.ts
+++ 63. Unique Paths II.ts
@@ -1,16 +1,18 @@
 function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
   const rowLen = obstacleGrid.length;
   const colLen = obstacleGrid[0].length;
+
   if (obstacleGrid[0][0] === 1) return 0;
+
   const dp = Array.from({ length: rowLen }, () => Array(colLen).fill(0));
   dp[0][0] = 1;
 
   for (let i = 1; i < colLen; i++) {
-    dp[0][i] = dp[0][i - 1] === 0 || obstacleGrid[0][i] === 1 ? 0 : 1;
+    dp[0][i] = obstacleGrid[0][i] === 1 || dp[0][i - 1] === 0 ? 0 : 1;
   }
 
   for (let j = 1; j < rowLen; j++) {
-    dp[j][0] = dp[j - 1][0] === 0 || obstacleGrid[j][0] === 1 ? 0 : 1;
+    dp[j][0] = obstacleGrid[j][0] === 1 || dp[j - 1][0] === 0 ? 0 : 1;
   }
 
   for (let i = 1; i < rowLen; i++) {
@@ -18,8 +20,6 @@ function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
       dp[i][j] = obstacleGrid[i][j] === 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
     }
   }
-
-  console.log(dp);
 
   return dp[rowLen - 1][colLen - 1];
 }
