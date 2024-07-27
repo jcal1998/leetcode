d8c7f36fa9542883a5de475b77c54e52e2138967
José Carlos Lee <josecarlos_lee@hotmail.com>
2024-07-27 18:24:35 -0300

    meeting rooms 2

--- 253. Meeting Rooms II.ts
+++ 253. Meeting Rooms II.ts
@@ -1,1 +1,25 @@
-function minMeetingRooms(intervals: number[][]): number {}
+function minMeetingRooms(intervals: number[][]): number {
+  const startTimes = [];
+  const endTimes = [];
+
+  for (const [start, end] of intervals) {
+    startTimes.push(start);
+    endTimes.push(end);
+  }
+
+  startTimes.sort((a, b) => a - b);
+  endTimes.sort((a, b) => a - b);
+
+  let rooms = 0;
+  let endPointer = 0;
+
+  for (let startPointer = 0; startPointer < startTimes.length; startPointer++) {
+    if (startTimes[startPointer] < endTimes[endPointer]) {
+      rooms++;
+    } else {
+      endPointer++;
+    }
+  }
+
+  return rooms;
+}
