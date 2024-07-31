680319d44d65d63f1fc4d1abd1c3527007530620
José Carlos Lee <josecarlos_lee@hotmail.com>
2024-07-31 01:48:29 -0300

    employee importance

---
+++ 690. Employee Importance.ts
@@ -0,0 +1,37 @@
+function getImportance(employees: Employee[], id: number): number {
+  const employeeMap = new Map<number, Employee>();
+  for (const employee of employees) {
+    employeeMap.set(employee.id, employee);
+  }
+
+  function dfs(empId: number): number {
+    const employee = employeeMap.get(empId)!;
+    let totalImportance = employee.importance;
+    for (const subId of employee.subordinates) {
+      totalImportance += dfs(subId);
+    }
+    return totalImportance;
+  }
+
+  return dfs(id);
+}
+function getImportanceBFS(employees: Employee[], id: number): number {
+  const employeeMap = new Map<number, Employee>();
+  for (const employee of employees) {
+    employeeMap.set(employee.id, employee);
+  }
+
+  let totalImportance = 0;
+  const queue: number[] = [id];
+
+  while (queue.length > 0) {
+    const currentId = queue.shift()!;
+    const employee = employeeMap.get(currentId)!;
+    totalImportance += employee.importance;
+    for (const subId of employee.subordinates) {
+      queue.push(subId);
+    }
+  }
+
+  return totalImportance;
+}
